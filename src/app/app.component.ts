3c6c807ebd300f8f5ba238cef932f6f294e08508
valenti-casas-ilerna <vcasas@ilerna.com>
2020-11-15 16:56:57 +0100

    Proyecto en JS de Phaser3

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import '../app/Phaser3/PhaserSampleGame.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+
+  constructor() {
+
+  }
+
+}
+
+
